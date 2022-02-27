c150d245f3e01395aa362df86663e75b6c1553f2
Artiom Uradau <70811102+hotlovestl@users.noreply.github.com>
2022-02-27 19:50:33 +0300

    Update word.model.js

--- src/resources/words/word.model.js
+++ src/resources/words/word.model.js
@@ -6,17 +6,9 @@ const WordsSchema = new Schema(
   {
     group: { type: Number, required: true },
     page: { type: Number, required: true },
-    word: { type: String, required: true, max: 100 },
-    image: { type: String, required: false, max: 150 },
-    audio: { type: String, required: false, max: 150 },
-    audioMeaning: { type: String, required: false, max: 150 },
-    audioExample: { type: String, required: false, max: 150 },
-    textMeaning: { type: String, required: false, max: 300 },
-    textExample: { type: String, required: false, max: 300 },
-    transcription: { type: String, required: false, max: 100 },
-    wordTranslate: String,
-    textMeaningTranslate: String,
-    textExampleTranslate: String
+    name: { type: String, required: true, max: 100 },
+    price: { type: Number, required: true, max: 100 },
+    unit: { type: String, required: true, max: 100 },
   },
   { collection: 'words' }
 );
